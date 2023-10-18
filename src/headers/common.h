2f6443ffd8e00b42e548e1c1baa3264c7e71075b
pacifiquem <Murangwa Pacifique>
2023-10-18 08:53:13 +0200

    [refactor]: copyright and author in source files

--- src/headers/common.h
+++ src/headers/common.h
@@ -1,3 +1,10 @@
+/*  
+    Copyright (c) MURANGWA Pacifique. and affiliates.
+    This source code is licensed under the Apache License 2.0 found in the
+    LICENSE file in the root directory of this source tree.
+*/
+
+
 #ifndef _KIN_COMMON_H
 #define _KIN_COMMON_H
 
--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,10 @@
+/*  
+    Copyright (c) MURANGWA Pacifique. and affiliates.
+    This source code is licensed under the Apache License 2.0 found in the
+    LICENSE file in the root directory of this source tree.
+*/
+
+
 /* core libraries */
 #include <stdio.h>
 
