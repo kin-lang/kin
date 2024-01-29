7942c3578e827aedb4c171af66518f40b9dce71e
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-01-29 11:03:31 +0200

    feat: entery point for interpreter

---
+++ src/utils/log.ts
@@ -0,0 +1,12 @@
+/**********************************************
+ *       Kin Programming Language             *
+ *                                            *
+ * Author: Copyright (c) MURANGWA Pacifique   *
+ *          and affiliates.                   *
+ * Description: Write computer programs in    *
+ *              Kinyarwanda.                  *
+ * License: Apache License 2.0                *
+ *********************************************/
+
+export const LogMessage = console.log;
+export const LogError = console.error;
